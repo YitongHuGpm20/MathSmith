d5ef1526192fcec7591920fa3de55250e2dbf5dc
YitongHuGpm20 <43768506+YitongHuGpm20@users.noreply.github.com>
2026-08-09 10:48:25 -0700

    FIxed step formats

--- Scripts/Gameplay/StepGenerator.gd
+++ Scripts/Gameplay/StepGenerator.gd
@@ -10,33 +10,35 @@ func GenerateAdditionSteps(expression: String) -> Array[String]:
 
 	var firstNumber := int(parts[0].strip_edges())
 	var secondNumber := int(parts[1].strip_edges())
-
 	var firstTens := (firstNumber / 10) * 10
 	var firstOnes := firstNumber % 10
-
 	var secondTens := (secondNumber / 10) * 10
 	var secondOnes := secondNumber % 10
-
 	var tensTotal := firstTens + secondTens
 	var onesTotal := firstOnes + secondOnes
 	var finalAnswer := firstNumber + secondNumber
 
 	var steps: Array[String] = [
-		"%d + %d" % [firstNumber, secondNumber],
-		"(%d + %d) + (%d + %d)" % [
+		"= (%d + %d) + (%d + %d)" % [
 			firstTens,
 			firstOnes,
 			secondTens,
 			secondOnes
 		],
-		"(%d + %d) + (%d + %d)" % [
+
+		"= (%d + %d) + (%d + %d)" % [
 			firstTens,
 			secondTens,
 			firstOnes,
 			secondOnes
 		],
-		"%d + %d" % [tensTotal, onesTotal],
-		str(finalAnswer)
+
+		"= %d + %d" % [
+			tensTotal,
+			onesTotal
+		],
+
+		"= %d" % finalAnswer
 	]
 
 	return steps
